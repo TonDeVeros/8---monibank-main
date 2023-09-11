b05dad3a8019aa4019a915cf00b9b15709e9288f
Ton DeVeros <TonDeveros@gmail.com>
2023-09-11 18:27:40 -0300

    git init

---
+++ js/validarIdade.js
@@ -0,0 +1,17 @@
+export default function ehMaiorDeIdade(campo) {
+    const dataNascimento = new Date(campo.value);
+
+    // validarIdade(dataNascimento);
+    // console.log(validarIdade(dataNascimento))
+
+    if(!validarIdade(dataNascimento)){
+        campo.setCustomValidity("O usuário não é maior de idade.");
+    }
+}
+
+function validarIdade(dataNascimento){
+    const dataAtual = new Date();
+    const dataMais18 = new Date(dataNascimento.getUTCFullYear() + 18, dataNascimento.getUTCMonth(), dataNascimento.getUTCDate());
+
+    return dataAtual >= dataMais18;
+}
